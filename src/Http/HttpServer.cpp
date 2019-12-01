6992287e1215f4130c5b8b57834211bd2e5cd7bd
Marcel Mathis <marcel.mathis@komaxgroup.com>
2019-12-01 10:01:37 +0100

    Delete MQTT from project

--- src/Http/HttpServer.cpp
+++ src/Http/HttpServer.cpp
@@ -83,7 +83,7 @@ void Session::doWrite()
 {
     auto self(shared_from_this());
     boost::asio::async_write(m_socket, boost::asio::buffer(m_txData, strlen(m_txData)),
-                             [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                             [self](boost::system::error_code ec, std::size_t /*length*/) {
                                  if (!ec) {
                                      // doRead();
                                  }
